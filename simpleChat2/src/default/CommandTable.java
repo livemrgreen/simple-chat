4db018c3a2dcb43f6a2fdcc70948e493903e53d3
Yannick Paz <livemrgreen@gmail.com>
2012-12-12 10:32:32 +0100

    ajouts a l'interface CommandTable

--- simpleChat2/src/default/CommandTable.java
+++ simpleChat2/src/default/CommandTable.java
@@ -1,4 +1,7 @@
 
 public interface CommandTable {
-
+	public static final String quit 	= 		"#quit";
+	public static final String logoff 	= 		"#logoff";
+	public static final String login 	= 		"#login";
+	public static final String getPort 	= 		"#getport";
 }
