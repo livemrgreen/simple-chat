ee90a2bee9376fd70cd554cf1f9c6c2ac449ec50
Yannick Paz <livemrgreen@gmail.com>
2012-12-19 12:32:29 +0100

    creation des classes des design patterns

---
+++ simpleChat3/src/default/client/AdaptableClient.java
@@ -0,0 +1,18 @@
+package client;
+
+import ocsf.client.*;
+
+public class AdaptableClient extends AbstractClient {
+
+	public AdaptableClient(String host, int port) {
+		super(host, port);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void handleMessageFromServer(Object msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ simpleChat3/src/default/client/OservableChat.java
@@ -0,0 +1,7 @@
+package client;
+
+import java.util.Observable;
+
+public class OservableChat extends Observable {
+
+}
