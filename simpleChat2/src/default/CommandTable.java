a762dc41ebd2b0f147aed2e9d70ca7f2d4be48a2
gwendalavir <gwendalavir@hotmail.com>
2012-12-19 11:54:06 +0100

    suppression commandeTable doublon dans default

--- simpleChat2/src/default/CommandTable.java
+++
@@ -1,7 +0,0 @@
-
-public interface CommandTable {
-	public static final String quit 	= 		"#quit";
-	public static final String logoff 	= 		"#logoff";
-	public static final String login 	= 		"#login";
-	public static final String getPort 	= 		"#getport";
-}
