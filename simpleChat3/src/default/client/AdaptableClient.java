4d8b7f49caa5bfcc3bf1b727d168b31659984849
Yannick Paz <livemrgreen@gmail.com>
2012-12-20 11:25:13 +0100

    simpleChat3 - structuration

--- simpleChat3/src/default/client/AdaptableClient.java
+++ simpleChat3/src/default/client/AdaptableClient.java
@@ -12,62 +12,16 @@ public class AdaptableClient extends AbstractClient {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void handleMessageFromClientUI(String message) throws IOException {
-		// delegue le traitement du message a observableChat
-		try {
-			if (message.startsWith(CommandTable.quit) && this.isConnected()) {
-				this.quit();
-			} else if (message.startsWith(CommandTable.quit)
-					&& !this.isConnected()) {
-				System.exit(0);
-
-			} else if (message.startsWith(CommandTable.logoff)
-					&& this.isConnected()) {
-				sendToServer(message);
-				this.closeConnection();
-			} else if (message.startsWith(CommandTable.logoff)
-					&& !this.isConnected()) {
-				obs.display("You are already disconnected");
-			} else if (message.startsWith(CommandTable.login)
-					&& !this.isConnected()) {
-				this.openConnection();
-				sendToServer(message);
-			} else if (message.startsWith(CommandTable.login)
-					&& this.isConnected()) {
-				obs.display("You are already connected");
-			} else if (message.startsWith(CommandTable.getPort)) {
-				obs.display(String.valueOf(this.getPort()));
-			} else if (message.startsWith(CommandTable.setPort)
-					&& !this.isConnected()) {
-				this.setPort(Integer.parseInt(message.substring(9)));
-
-			} else if (message.startsWith(CommandTable.setPort)
-					&& this.isConnected()) {
-				obs.display("You are already connected. The port can not be changed");
-			} else if (message.startsWith(CommandTable.getHost)) {
-				obs.display(String.valueOf(this.getHost()));
-			} else if (message.startsWith(CommandTable.setHost)
-					&& !this.isConnected()) {
-				this.setHost(message.substring(9));
-			} else if (message.startsWith(CommandTable.setHost)
-					&& this.isConnected()) {
-				obs.display("You are already connected. The host can not be changed");
-			} else if (this.isConnected()) {
-				sendToServer(message);
-			} else {
-				obs.display("You need to enter your login to be connected to the chat. Please use the command : #login <login>");
-			}
-		} catch (IOException e) {
-			obs.display("Could not send message to server.  Terminating client.");
-			quit();
-		}
-	}
-
-	@Override
+//	<-----------------------------------------------
+//	<-----------------------------------------------
+	
 	protected void handleMessageFromServer(Object msg) {
 		// delegue le traitement du message a ObservableChat
 		obs.handleMessageFromServer(msg);
 	}
+	
+//	<-----------------------------------------------
+//	<-----------------------------------------------
 
 	/**
 	 * This method terminates the client.
--- simpleChat3/src/default/client/ObservableChat.java
+++ simpleChat3/src/default/client/ObservableChat.java
@@ -5,11 +5,11 @@ import java.util.Observable;
 
 public class ObservableChat extends Observable {
 	ChatClient obs;
-	
-	public void handleMessageFromClientUI(String message) throws IOException {
-//		delegue le traitement du message a observableChat
-	}
+	AdaptableClient adapt;
 
+//	<-----------------------------------------------
+//	<-----------------------------------------------
+	
 	public void handleMessageFromServer(Object msg) {
 		// delegue le traitement du message a chatClient via notify
 		this.notify(msg);
@@ -19,5 +19,75 @@ public class ObservableChat extends Observable {
 		// delegue le traitement du message aux observateurs via update
 		obs.update(this, msg);
 	}
+	
+//	<-----------------------------------------------
+//	<-----------------------------------------------
+	
+	
+//	----------------------------------------------->
+//	----------------------------------------------->
+
+	public void closeConnection() {
+		// TODO Auto-generated method stub
+		try {
+			adapt.closeConnection();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return adapt.isConnected();
+	}
+
+	public void sendToServer(String message) {
+		// TODO Auto-generated method stub
+		try {
+			adapt.sendToServer(message);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void openConnection() {
+		// TODO Auto-generated method stub
+		try {
+			adapt.openConnection();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public int getPort() {
+		// TODO Auto-generated method stub
+		return adapt.getPort();
+	}
+
+	public void setPort(int port) {
+		// TODO Auto-generated method stub
+		adapt.setPort(port);
+	}
+
+	public String getHost() {
+		// TODO Auto-generated method stub
+		return adapt.getHost();
+	}
+
+	public void setHost(String substring) {
+		// TODO Auto-generated method stub
+		adapt.setHost(substring);
+	}
+
+	public void quit() {
+		// TODO Auto-generated method stub
+		adapt.quit();
+	}
+	
+//	----------------------------------------------->
+//	----------------------------------------------->
 
 }
